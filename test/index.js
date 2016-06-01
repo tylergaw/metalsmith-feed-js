9095b00e2b7a81e5553bbbbc61bd93b189d0ff3b
Tyler Gaw <me@tylergaw.com>
2016-06-01 08:23:36 -0400

    Syntax; fixes and es6

--- test/index.js
+++ test/index.js
@@ -1,12 +1,13 @@
-var test = require('tape');
-var metalsmith = require('metalsmith')
-var collections = require('metalsmith-collections')
-var tags = require('metalsmith-tags')
-var feed = require('../metalsmith-feed')
+'use strict';
 
-var parseString = require('xml2js').parseString
+const test = require('tape');
+const metalsmith = require('metalsmith');
+const collections = require('metalsmith-collections');
+const tags = require('metalsmith-tags');
+const feed = require('../metalsmith-feed');
+const parseString = require('xml2js').parseString
 
-var metadata = {
+const metadata = {
   site: {
     title: 'Example',
     url: 'http://www.example.org',
@@ -14,7 +15,7 @@ var metadata = {
   }
 }
 
-test('it renders rss feed', function (t) {
+test('it renders rss feed', (t) => {
   metalsmith('test/fixtures')
     .metadata(metadata)
     .use(collections({
@@ -23,43 +24,43 @@ test('it renders rss feed', function (t) {
     .use(feed({
       collection: 'posts'
     }))
-    .build(function(err, files) {
+    .build((err, files) => {
       t.ok(!err, 'should be no error building')
 
-      parseString(files['rss.xml'].contents, function(err, result) {
+      parseString(files['rss.xml'].contents, (err, result) => {
         t.ok(!err, 'should be no error on parsing rss')
         t.ok(result, 'there should be some rss content')
 
         t.equal(result['rss']['$']['xmlns:atom'], 'http://www.w3.org/2005/Atom')
 
-        channel = result['rss']['channel'][0]
-        t.equal(channel.title[0], metadata.site.title)
-        t.equal(channel.author[0], metadata.site.author)
-        t.equal(channel.item.length, 1)
+        const channel = result['rss']['channel'][0];
+        t.equal(channel.title[0], metadata.site.title);
+        t.equal(channel.author[0], metadata.site.author);
+        t.equal(channel.item.length, 1);
 
-        post = channel.item[0]
-        t.equal(post.title[0], 'Theory of Juice')
-        t.equal(post.description[0], '<p>juice appeal</p>\n')
+        const post = channel.item[0];
+        t.equal(post.title[0], 'Theory of Juice');
+        t.equal(post.description[0], '<p>juice appeal</p>\n');
 
-        t.end()
+        t.end();
       })
     })
 })
 
-test('it complains if no tags or collections', function(t) {
+test('it complains if no tags or collections', (t) => {
   metalsmith('test/fixtures')
     .metadata(metadata)
     .use(feed({
       collection: 'posts'
     }))
-    .build(function(err, files) {
-      t.equal(err.message, 'no collections or tags configured - see metalsmith-collections or metalsmith-tags', 'no collections or tags configured - see metalsmith-collections or metalsmith-tags')
-      t.end()
+    .build((err, files) => {
+      t.equal(err.message, 'no collections or tags configured - see metalsmith-collections or metalsmith-tags', 'no collections or tags configured - see metalsmith-collections or metalsmith-tags');
+      t.end();
     })
 })
 
-test('it complains with not site.url', function(t) {
-  delete metadata.site.url
+test('it complains with not site.url', (t) => {
+  delete metadata.site.url;
   metalsmith('test/fixtures')
     .metadata(metadata)
     .use(collections({
@@ -68,8 +69,8 @@ test('it complains with not site.url', function(t) {
     .use(feed({
       collection: 'posts'
     }))
-    .build(function(err, files) {
-      t.equal(err.message, 'either site_url or metadata.site.url must be configured', 'either site_url or metadata.site.url must be configured')
-      t.end()
+    .build((err, files) => {
+      t.equal(err.message, 'either site_url or metadata.site.url must be configured', 'either site_url or metadata.site.url must be configured');
+      t.end();
     })
 })
