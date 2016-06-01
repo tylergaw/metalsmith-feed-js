1be72b1e2c5aee2a8c35ace8af8f9b93f2f817dc
Tyler Gaw <me@tylergaw.com>
2016-06-01 13:43:24 -0400

    Allow Functions for feed item options (#1)
    * Use fixed dependency versions
    * Syntax; fixes and es6
    * Allow for itemDataHandlers
    - Add fixtures with nested author data and no author data
    - Write test cases for nested author data and no author data
    - Update metalsmith-feed to allow for custom item data handler functions
    - Document additions
    - General cleanup/style opinion changes

--- test/index.js
+++ test/index.js
@@ -1,65 +1,94 @@
-var test = require('tape');
-var metalsmith = require('metalsmith')
-var collections = require('metalsmith-collections')
-var tags = require('metalsmith-tags')
-var feed = require('../metalsmith-feed')
+'use strict';
 
-var parseString = require('xml2js').parseString
+const test = require('tape');
+const metalsmith = require('metalsmith');
+const collections = require('metalsmith-collections');
+const tags = require('metalsmith-tags');
+const feed = require('../metalsmith-feed');
+const parseString = require('xml2js').parseString
 
-var metadata = {
+const metadata = {
   site: {
     title: 'Example',
     url: 'http://www.example.org',
     author: 'Philodemus'
   }
-}
+};
 
-test('it renders rss feed', function (t) {
+test('it renders rss feed', (t) => {
   metalsmith('test/fixtures')
     .metadata(metadata)
     .use(collections({
       posts: '*.html'
     }))
     .use(feed({
-      collection: 'posts'
+      collection: 'posts',
+      itemDataHandlers: {
+        author: (file, defaultValue) => {
+          const author = file.author;
+
+          if (!author) {
+            return defaultValue;
+          }
+
+          if (typeof author === 'string') {
+            return author;
+          } else {
+            return author.name;
+          }
+        }
+      }
     }))
-    .build(function(err, files) {
+    .build((err, files) => {
       t.ok(!err, 'should be no error building')
-      
-      parseString(files['rss.xml'].contents, function(err, result) {
+
+      parseString(files['rss.xml'].contents, (err, result) => {
         t.ok(!err, 'should be no error on parsing rss')
         t.ok(result, 'there should be some rss content')
 
         t.equal(result['rss']['$']['xmlns:atom'], 'http://www.w3.org/2005/Atom')
-        
-        channel = result['rss']['channel'][0]
-        t.equal(channel.title[0], metadata.site.title)
-        t.equal(channel.author[0], metadata.site.author)
-        t.equal(channel.item.length, 1)
 
-        post = channel.item[0]
-        t.equal(post.title[0], 'Theory of Juice')
-        t.equal(post.description[0], '<p>juice appeal</p>\n')
-        
-        t.end()
+        const channel = result['rss']['channel'][0];
+        t.equal(channel.title[0], metadata.site.title);
+        t.equal(channel.author[0], metadata.site.author);
+        t.equal(channel.item.length, 3);
+
+        const post1 = channel.item[0];
+        t.equal(post1.title[0], 'Theory of Juice');
+        t.equal(post1.description[0], '<p>juice appeal</p>\n');
+        t.equal(post1['dc:creator'][0], 'Joe Mosely');
+
+        // Test custom itemDataHandlers with a nested author value
+        const post2 = channel.item[1];
+        t.equal(post2.title[0], 'My Second Post');
+        t.equal(post2.description[0], '<p>She don\'t like her boring job, no!</p>\n');
+        t.equal(post2['dc:creator'][0], 'Janie Jones');
+
+        // Test default author
+        const post3 = channel.item[2];
+        t.equal(post3.title[0], 'All The Defaults');
+        t.equal(post3.description[0], '<p>Test for defaults</p>\n');
+        t.equal(post3['dc:creator'][0], metadata.site.author);
+
+        t.end();
       })
     })
 })
 
-test('it complains if no tags or collections', function(t) {
+test('it complains if no tags or collections', (t) => {
   metalsmith('test/fixtures')
     .metadata(metadata)
     .use(feed({
       collection: 'posts'
     }))
-    .build(function(err, files) {
-      t.equal(err.message, 'no collections or tags configured - see metalsmith-collections or metalsmith-tags', 'no collections or tags configured - see metalsmith-collections or metalsmith-tags')
-      t.end()
+    .build((err, files) => {
+      t.equal(err.message, 'no collections or tags configured - see metalsmith-collections or metalsmith-tags', 'no collections or tags configured - see metalsmith-collections or metalsmith-tags');
+      t.end();
     })
 })
 
-test('it complains with not site.url', function(t) {
-  delete metadata.site.url
+test('it complains with not site.url', (t) => {
+  delete metadata.site.url;
   metalsmith('test/fixtures')
     .metadata(metadata)
     .use(collections({
@@ -68,10 +97,8 @@ test('it complains with not site.url', function(t) {
     .use(feed({
       collection: 'posts'
     }))
-    .build(function(err, files) {
-      t.equal(err.message, 'either site_url or metadata.site.url must be configured', 'either site_url or metadata.site.url must be configured')
-      t.end()
+    .build((err, files) => {
+      t.equal(err.message, 'either site_url or metadata.site.url must be configured', 'either site_url or metadata.site.url must be configured');
+      t.end();
     })
 })
-
-
